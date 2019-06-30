fc4e54bedb0ceae2a891ceaca0f8b20a981704fc
Zach  Smith <zdsmith001@gmail.com>
2019-06-30 15:39:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,8 +5,8 @@ def turn_count(board1)
   board1.each do |position|
     if position == "X" || position == "O"
       counter += 1
-      puts counter
-      return counter
     end
   end
+  puts counter
+  return counter
 end
