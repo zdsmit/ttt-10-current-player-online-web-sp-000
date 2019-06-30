96a9cfe0c40a9587f7c73e29a5e332a45dda24ec
Zach  Smith <zdsmith001@gmail.com>
2019-06-30 15:49:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,11 +12,11 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board)) % 2 == 0
-    return "O"
-    puts "O"
-  else
+  if !(turn_count(board)) % 2 == 0
     return "X"
     puts "X"
+  else
+    return "O"
+    puts "O"
   end
 end
