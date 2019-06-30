89ebb72d44936255385fa106c9c38c22f1b6e2ea
Zach  Smith <zdsmith001@gmail.com>
2019-06-30 15:38:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 board1 = ["X", " ", " ", "O", " ", " ", "X", " ", " "]
 
 def turn_count(board1)
+  counter = 0
   board1.each do |position|
-    counter = 0
     if position == "X" || position == "O"
       counter += 1
       puts counter
